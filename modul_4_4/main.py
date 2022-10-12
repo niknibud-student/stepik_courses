9370e2117d91d49bd8adba5ca47527500787a01b
Pavel Dubinkin <pdubinkin@yandex.ru>
2022-10-12 19:15:20 +0300

    Добавил фотки для мемов и проверил библиотеку

---
+++ modul_4_4/main.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def make_meme(image, top_text='', bottom_text=''):
+    pass
+
+def main():    
+    with Image.open("templates/boromir.jpg") as image:
+        image.show()
+
+
+if __name__ == '__main__':
+    main()
