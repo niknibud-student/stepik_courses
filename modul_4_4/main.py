9f9466446c44c58cd59788a4be5ed6cff3cb7d2a
Pavel Dubinkin <pdubinkin@yandex.ru>
2022-10-12 19:55:54 +0300

    Добавил bottom_text

--- modul_4_4/main.py
+++ modul_4_4/main.py
@@ -1,16 +1,27 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def make_meme(image, top_text='', bottom_text=''):
+def draw_text(image, type, text):
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype('impact.ttf', 50)
     white_color = (255, 255, 255)
-    text_width = draw.textlength(top_text, font)  # ширина данного текста с данным шрифтом
-    draw.text((image.width / 2 - text_width / 2, 10), top_text, white_color, font)
+    # ширина и высота данного текста с данным шрифтом
+    text_width = draw.textlength(text, font) 
+    text_height = draw.textsize(text, font)[1]
+    # зависит от расположения
+    y_start = 10 if type == 'top' else image.height - text_height - 10
+    draw.text((image.width / 2 - text_width / 2, y_start), 
+              text, white_color, font)
+
+def make_meme(image, top_text='', bottom_text=''):
+    draw_text(image, 'top', top_text)
+    draw_text(image, 'bottom', bottom_text)
     return image
 
 def main():    
     with Image.open("templates/boromir.jpg") as image:
-        image = make_meme(image, 'Sample Text')
+        image = make_meme(image, 
+                          'Нельзя так просто взять', 
+                          'и сразу написать идеальное приложение')
         image.save('meme_result.jpg')
         image.show()
 
